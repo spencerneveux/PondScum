6e7534057aa5abb6bc1923ab1b4f8832fceb008e
spencerneveux <spencerneveux@gmail.com>
2019-02-02 12:24:42 -0800

    Changing method names

--- src/PondScum.java
+++ src/PondScum.java
@@ -11,24 +11,48 @@ public class PondScum {
     private static int numColumns = 0;
 
     public static void main(String[] args) {
+        //Determine configuration of pond
+        getPondConfiguration();
+        //Create grid according to configuration
+        createGrid();
+
+        //How to solve system of linear equations using matrix
         double[][] lArray = {{4, -1} , {-1, 4}};
         double[] rArray = {602, 1303};
         Matrix A = new Matrix(lArray);
         Matrix B = new Matrix(rArray, 2);
         Matrix x = A.solve(B);
         System.out.println(Arrays.deepToString(x.getArray()));
-
-        getPondConfiguration();
-        textReader();
     }
 
     /**
-     * Method to create grid layout to represent pond 
+     * Method to determine number of rows/columns
      */
-    private static void textReader() {
+    private static void getPondConfiguration() {
+        try(Scanner input = new Scanner(new File("src/example.txt"))) {
+            while (input.hasNextLine()) {
+                //If another line then add to num Rows
+                numRows++;
+                //Seperate values to determine num columns
+                String line = input.nextLine();
+                String[] stringValues = line.split(",");
+                numColumns = stringValues.length;
+            }
+            System.out.println("Number of Rows = " + numRows + " Number of Columns = " + numColumns);
+        }catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+    }
+
+    /**
+     * Method to create grid layout to represent pond
+     */
+    private static double[][] createGrid() {
+        //Create grid to be returned
+        double[][] grid = new double[numRows][numColumns];
+        //Scan through text file
         try(Scanner input = new Scanner(new File("src/example.txt"))){
             int rowCount = 0;
-            int[][] grid = new int[numRows][numColumns];
             while(input.hasNextLine()) {
                 //Read each line and separate values
                 String line = input.nextLine();
@@ -40,40 +64,18 @@ public class PondScum {
                         stringValues[i] = "1";
                 }
 
-                //Add values to grid in integer form
+                //Add values to grid in double form
                 for (int i = 0; i < stringValues.length; i++) {
-                    grid[rowCount][i] = Integer.parseInt(stringValues[i]);
+                    grid[rowCount][i] = Double.parseDouble(stringValues[i]);
                 }
                 rowCount++;
             }
             //Print out grid
             System.out.println(Arrays.deepToString(grid));
-
         }catch (FileNotFoundException ex) {
             ex.getMessage();
         }
+        return grid;
     }
-
-    /**
-     * Method to determine number of rows/columns
-     */
-    private static void getPondConfiguration() {
-        try(Scanner input = new Scanner(new File("src/example.txt"))) {
-
-            while (input.hasNextLine()) {
-                //If another line then add to num Rows
-                numRows++;
-                //Seperate values to determine num columns
-                String line = input.nextLine();
-                String[] stringValues = line.split(",");
-                numColumns = stringValues.length;
-            }
-
-            System.out.println("Number of Rows = " + numRows + " Number of Columns = " + numColumns);
-        }catch (FileNotFoundException e) {
-            e.getMessage();
-        }
-    }
-
 
 }
