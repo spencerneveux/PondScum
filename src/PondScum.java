5d723f6c3f1e2206e73a3888dc04b84a0133744f
spencerneveux <spencerneveux@gmail.com>
2019-02-02 12:06:45 -0800

    Initial commit

---
+++ src/PondScum.java
@@ -0,0 +1,79 @@
+import Jama.Matrix;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PondScum {
+    private static int numRows = 0;
+    private static int numColumns = 0;
+
+    public static void main(String[] args) {
+        double[][] lArray = {{4, -1} , {-1, 4}};
+        double[] rArray = {602, 1303};
+        Matrix A = new Matrix(lArray);
+        Matrix B = new Matrix(rArray, 2);
+        Matrix x = A.solve(B);
+        System.out.println(Arrays.deepToString(x.getArray()));
+
+        getPondConfiguration();
+        textReader();
+    }
+
+    /**
+     * Method to create grid layout to represent pond 
+     */
+    private static void textReader() {
+        try(Scanner input = new Scanner(new File("src/example.txt"))){
+            int rowCount = 0;
+            int[][] grid = new int[numRows][numColumns];
+            while(input.hasNextLine()) {
+                //Read each line and separate values
+                String line = input.nextLine();
+                String[] stringValues = line.split(",");
+
+                //Find variable height ponds
+                for (int i = 0; i < stringValues.length; i++) {
+                    if(stringValues[i].charAt(0) == '!')
+                        stringValues[i] = "1";
+                }
+
+                //Add values to grid in integer form
+                for (int i = 0; i < stringValues.length; i++) {
+                    grid[rowCount][i] = Integer.parseInt(stringValues[i]);
+                }
+                rowCount++;
+            }
+            //Print out grid
+            System.out.println(Arrays.deepToString(grid));
+
+        }catch (FileNotFoundException ex) {
+            ex.getMessage();
+        }
+    }
+
+    /**
+     * Method to determine number of rows/columns
+     */
+    private static void getPondConfiguration() {
+        try(Scanner input = new Scanner(new File("src/example.txt"))) {
+
+            while (input.hasNextLine()) {
+                //If another line then add to num Rows
+                numRows++;
+                //Seperate values to determine num columns
+                String line = input.nextLine();
+                String[] stringValues = line.split(",");
+                numColumns = stringValues.length;
+            }
+
+            System.out.println("Number of Rows = " + numRows + " Number of Columns = " + numColumns);
+        }catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+    }
+
+
+}
